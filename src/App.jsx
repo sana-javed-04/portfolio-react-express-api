ab00c88bed587c64cbb4a00be7ef9e14a021e9b2
Sana Javed <wpress0411@gmail.com>
2026-07-07 08:51:33 +0500

    correct certificate section position

--- src/App.jsx
+++ src/App.jsx
@@ -67,13 +67,13 @@ function App() {
 
         <About />
 
+        <Certificates />
+
         <Skills />
 
         <Projects />
 
         <MiniProjects />
-
-        <Certificates />
 
         <Blog />
 
